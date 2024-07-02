9635ab595bd525d4b58757a9e96165b42e2640bd
Augustine <mayakiaugustine0@gmail.com>
2024-07-02 12:07:21 +0100

    code refix

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 let currentDay = document.querySelector(".currentDay");
 let currentTime = document.querySelector(".utcTime");
 const days = [
-  "Sunday:",
-  "Monday:",
-  "Tuesday:",
-  "Wednesday:",
-  "Thursday:",
-  "Friday:",
-  "Saturday:",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 const date = new Date();
 let day = days[date.getDay()];
 
-currentDay.innerHTML = day;
-currentTime.innerHTML = date.getTime();
+currentDay.innerHTML = `Current day: ${day}`;
+currentTime.innerHTML = `UTC: ${date.toUTCString()}`;
